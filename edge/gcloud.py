b98835468e0e767d5094263fd6cf2f8657516bc1
D. Lowl <d.lowl@d-lowl.space>
2021-07-13 13:45:29 +0500

    Fix gcloud region list

--- edge/gcloud.py
+++ edge/gcloud.py
@@ -2,8 +2,8 @@ import subprocess
 from typing import List
 
 
-def get_gcp_regions() -> List[str]:
-    result = subprocess.check_output("gcloud compute regions list", shell=True).decode("utf-8")
+def get_gcp_regions(project: str) -> List[str]:
+    result = subprocess.check_output(f"gcloud compute regions list --project {project}", shell=True).decode("utf-8")
     rows = result.split("\n")[1:-1]
     regions = [row.split(" ")[0] for row in rows]
     return regions
