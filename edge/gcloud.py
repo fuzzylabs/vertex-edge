3b48036f5f6e7c9d931806960ffb5b0934447627
D. Lowl <d.lowl@d-lowl.space>
2021-07-14 16:54:02 +0500

    Hard-code regions that are supported by Vertex AI

--- edge/gcloud.py
+++ edge/gcloud.py
@@ -1,9 +1,22 @@
-import subprocess
 from typing import List
+
+# Regions that are supported for Vertex AI training and deployment
+regions = [
+    "us-central1",
+    "europe-west4",
+    "asia-east1",
+    "asia-northeast1",
+    "asia-northeast3",
+    "asia-southeast1",
+    "australia-southeast1",
+    "europe-west1",
+    "europe-west2",
+    "northamerica-northeast1",
+    "us-west1",
+    "us-east1",
+    "us-east4",
+]
 
 
 def get_gcp_regions(project: str) -> List[str]:
-    result = subprocess.check_output(f"gcloud compute regions list --project {project}", shell=True).decode("utf-8")
-    rows = result.split("\n")[1:-1]
-    regions = [row.split(" ")[0] for row in rows]
     return regions
