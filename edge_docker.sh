79813ee7917a7f16d9ff99d9932a076965fe2f25
D. Lowl <d.lowl@d-lowl.space>
2021-07-15 10:48:22 +0500

    Pass command in edge_docker.sh

--- edge_docker.sh
+++ edge_docker.sh
@@ -5,4 +5,4 @@ docker run -it \
  -v "$(pwd)":/project/ \
  -v ~/.config/gcloud/:/root/.config/gcloud/ \
  -e ACCOUNT="$ACCOUNT" \
- --entrypoint bash edge ./edge_docker_entrypoint.sh
+ --entrypoint bash edge ./edge_docker_entrypoint.sh "$@"
--- edge_docker_entrypoint.sh
+++ edge_docker_entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export GOOGLE_APPLICATION_CREDENTIALS=/root/.config/gcloud/application_default_credentials.json
 gcloud config set account "$ACCOUNT"
-python edge.py setup
+python edge.py "$@"
