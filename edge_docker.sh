d151f592301689054b5e1aa327b2c8d300a502be
D. Lowl <d.lowl@d-lowl.space>
2021-07-16 12:14:46 +0500

    Fix .dvc permissions in edge_docker.sh

--- edge_docker.sh
+++ edge_docker.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 ACCOUNT=$(gcloud config get-value account)
-
+HOST_UID=$(id -u)
+HOST_GID=$(id -g)
 docker run -it \
  -v "$(pwd)":/project/ \
  -v ~/.config/gcloud/:/root/.config/gcloud/ \
- -e ACCOUNT="$ACCOUNT" \
+ -e ACCOUNT="$ACCOUNT" -e HOST_UID="$HOST_UID" -e HOST_GID="$HOST_GID" \
  --entrypoint bash edge ./edge_docker_entrypoint.sh "$@"
