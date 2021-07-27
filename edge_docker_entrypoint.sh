3246dcdfced946145f1dd1712308b887d4d5d232
archena <archena@users.noreply.github.com>
2021-07-27 08:44:44 +0100

    Updating README to cover Docker usage

--- edge_docker_entrypoint.sh
+++ edge_docker_entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 export GOOGLE_APPLICATION_CREDENTIALS=/root/.config/gcloud/application_default_credentials.json
 gcloud config set account "$ACCOUNT"
-python edge.py "$@"
+
+if [[ $1 == "bash" ]]
+then
+   bash
+else
+    python edge.py "$@"
+fi
+
 chown -R "$HOST_UID":"$HOST_GID" .dvc/
