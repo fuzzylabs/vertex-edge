7845be8580834ff9c0dcc3c5d90b135caff45677
Matt Squire <archena@users.noreply.github.com>
2021-07-29 08:37:11 +0100

    Fix terminal rendering by restoring the -t flag for Docker. Remove \r characters in outputs from edge.sh before substituting them into strings (#38)

--- test_endpoint.sh
+++ test_endpoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ENDPOINT=$(./edge.sh model get-endpoint)
-REGION=$(./edge.sh config get-region)
+ENDPOINT=$(./edge.sh model get-endpoint | tr -d '\r')
+REGION=$(./edge.sh config get-region | tr -d '\r')
 curl \
 -X POST \
 -H "Authorization: Bearer $(gcloud auth print-access-token)" \
