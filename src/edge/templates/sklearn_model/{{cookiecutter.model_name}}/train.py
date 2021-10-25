ada634b0b9a54a2ba3b730624f2943e08b9b00c9
archena <archena@users.noreply.github.com>
2021-10-25 11:52:58 +0100

    Update Cookie Cutter template

--- src/edge/templates/sklearn_model/{{cookiecutter.model_name}}/train.py
+++ src/edge/templates/sklearn_model/{{cookiecutter.model_name}}/train.py
@@ -1,27 +1,11 @@
-from edge.training.training import *
-from edge.training.sklearn.utils import save_results
-from edge.sacred import track_experiment
-from sacred import Experiment
+from edge.train import Trainer
 
-_config, state = get_config_and_state()
-ex = Experiment("{{cookiecutter.model_name}}-model-training", save_git_info=False)
+class MyTrainer(Trainer):
+    def main(self):
+        self.set_parameter("example", 123)
 
-@ex.config
-def cfg():
-    # Add your config here
-    model_name = "{{cookiecutter.model_name}}"
-    is_vertex = False
+        # Add model training logic here
 
-@ex.automain
-@vertex_wrapper(_config, state)
-def run(_run,
-        strategy,
-        model_output_dir="./",
-):
-    # Add your model training code ehre
+        return 0 # return your model score here
 
-    metrics = {
-        "score": # Add model score here
-    }
-
-    save_results(dummy_clf, metrics, model_output_dir)
+MyTrainer("{{cookiecutter.model_name}}-model-training").run()
